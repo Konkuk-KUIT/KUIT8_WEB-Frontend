8e55fd79e456e127434c18c361afeae040ce1e83
Nayeon07 <kimny5726@gmail.com>
2026-09-24 21:23:47 +0900

    feat: add mission files

---
+++ week3/mission/src/App.jsx
@@ -0,0 +1,11 @@
+import Router from "./pages/Router";
+
+const App = () => {
+    return (
+        <div className="font-['Pretendard'] leading-[normal]">
+            <Router />
+        </div>
+    );
+};
+
+export default App;
---
+++ week3/mission/src/pages/Stores/Stores.jsx
@@ -0,0 +1,19 @@
+import BackBar from "../../components/BackBar";
+import OrderBar from "../../components/OrderBar/OrderBar";
+import StoreItem from "../../components/StoreItem";
+import stores from "../../models/stores";
+
+const Stores = () => {
+	return (
+		<main className="mt-[41px] mb-[77px] w-[390px]">
+			<BackBar orderCancel={false} />
+			<div className="fixed top-[41px] w-[390px] bg-white p-[26px_298px_2px_24px] text-[26px] font-bold text-[#191f28]">
+				샐러드
+			</div>
+			<StoreItem items={stores} />
+			<OrderBar />
+		</main>
+	);
+};
+
+export default Stores;
