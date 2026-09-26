12f17f1221e502ef11fe14dade7f34fd5c8b662a
일혁 <dnfjddk2@gmail.com>
2026-09-26 15:10:22 +0900

    feat: 3주차 실습 Tailwind CSS와 Pretendard 설정
    tailwindcss·@tailwindcss/vite 로 Vite 플러그인을 등록하고 index.css 에서 불러온다. Pretendard 는 main.jsx 에서 불러와 App 최상위 요소에 폰트와 기본 줄 높이를 적용한다.

--- week3/jeongilhyuk/src/App.jsx
+++ week3/jeongilhyuk/src/App.jsx
@@ -1,7 +1,11 @@
 import Router from "./pages/Router";
 
 const App = () => {
-	return <Router />;
+	return (
+		<div className="font-['Pretendard'] leading-[normal]">
+			<Router />
+		</div>
+	);
 };
 
 export default App;
